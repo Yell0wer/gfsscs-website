12f719072457b90bb2a7b6b21b11d2b25f60657f
Carl Zhang <zhang.carl.x@gmail.com>
2023-08-12 14:59:02 -0400

    hero barebones

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import './styles/App.css';
+import Hero from "./components/hero.component"
+
+function App() {
+    return (
+        <div className="App">
+            <Hero />
+        </div>
+    );
+}
+
+export default App;
---
+++ client/src/components/hero.component.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function Hero() {
+    return (
+        <div className="hero">
+            <h1>
+                <b className="hl">Glenforest S.S.</b><br />
+                Computer Science
+            </h1>
+            <h2 class="socials">
+                <a href="https://discord.gg/Uj85nUU5Fj" target="_blank" className="bi bi-discord"></a>
+                <a href="https://instagram.com" target="_blank" className="bi bi-instagram"></a>
+            </h2>
+        </div>
+    );
+}
