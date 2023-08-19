90e935ee58edafc7a6a0da17e6f68e90e3437aa8
Carl Zhang <zhang.carl.x@gmail.com>
2023-08-19 14:27:54 -0400

    updated client

---
+++ client1/app/layout.tsx
@@ -0,0 +1,24 @@
+import './globals.css'
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Glenforest SS Computer Science',
+  description: 'Welcome to Glenforest Secondary School\'s Computer Science Club!',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Epilogue:wght@400;700&family=Fira+Code:wght@400;700&display=swap" rel="stylesheet" />
+      </head>
+      <body className="bg-dark">{children}</body>
+    </html>
+  )
+}
